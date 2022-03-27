9666fa7072b8f8ab2961d883ff9054d85ee267a9
Kevin David Molina Gómez <kevin.molina.go@gmail.com>
2022-03-27 14:22:14 -0600

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Kevin Molina on 27/03/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
