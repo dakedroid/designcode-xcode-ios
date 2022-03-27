8cc07ebff31d8b97eb602997b179b6da05dceef0
Kevin David Molina Gómez <kevin.molina.go@gmail.com>
2022-03-27 16:09:05 -0600

    Diseño hasta un punto donde todavía le entiendo, antes de agregarle el blur

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,13 +9,123 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            
+            TitleView()
+              
+            
+            
+            CardBottomView()
+              
+            
+          
+            CardView()
+                .offset(x: 0, y: -40)
+                .scaleEffect(0.85)
+                .rotationEffect(Angle(degrees: 15))
+                .rotation3DEffect(Angle(degrees: 50), axis: /*@START_MENU_TOKEN@*/(x: 10.0, y: 10.0, z: 10.0)/*@END_MENU_TOKEN@*/)
+                .blendMode(.hardLight)
+            
+            CardView()
+                .offset(x: 0, y: -20)
+                .scaleEffect(0.9)
+                .rotationEffect(Angle(degrees: 10))
+                .rotation3DEffect(Angle(degrees: 40), axis: /*@START_MENU_TOKEN@*/(x: 10.0, y: 10.0, z: 10.0)/*@END_MENU_TOKEN@*/)
+                .blendMode(.hardLight)
+            
+            
+            CertificateView()
+                .scaleEffect(0.95)
+                .rotationEffect(Angle(degrees: 5))
+                .rotation3DEffect(Angle(degrees: 30), axis: /*@START_MENU_TOKEN@*/(x: 10.0, y: 10.0, z: 10.0)/*@END_MENU_TOKEN@*/)
+            
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            Text("Que onda")
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.blue)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct CertificateView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Que onda ")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("accent"))
+                        .padding(.top)
+                    Text("Certificate")
+                        .foregroundColor(Color.white)
+                }
+                Spacer()
+                Image("Logo").resizable()
+                    .frame(width: 30.0, height: 30.0)
+            }
+            .padding(.horizontal)
+            Spacer()
+            Image("Background")
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(10)
+        .shadow(radius: 20)
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Spacer()
+            }
+            Image("Illustration5")
+            Spacer()
+        }.padding()
+    
+    }
+}
+
+struct CardBottomView: View {
+    var body: some View {
+        VStack(spacing: 20.0) {
+            
+            Rectangle()
+                .frame(width: 60, height: 6 )
+                .cornerRadius(3.0)
+                .opacity(0.1)
+            
+            Text("Este es un texto algo largo que tiene bastante información")
+                .lineLimit(10)
+            
+            Spacer()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity )
+        .padding()
+        .padding(.horizontal)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(y: 580)
     }
 }
